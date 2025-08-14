a52403b3789e68450dbd28a7d51bdce5f4fb8c57
Jhziel <barnedodonjaziel57@gmail.com>
2025-08-14 20:06:05 +0800

    bug

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Job from "./Job";
 import Spinners from "./Spinners";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+  const location = useLocation();
 
   useEffect(() => {
     const API_URL = import.meta.env.VITE_API_URL;
@@ -22,7 +23,7 @@ const JobListings = ({ isHome = false }) => {
       }
     };
     getJobs();
-  }, ); // location.key changes every navigation
+  }, [location.state]); // location.key changes every navigation
 
   const latestJob = isHome ? jobs.slice(0, 3) : jobs;
 
