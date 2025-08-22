5572703ee1ae217b270638801343c0f532ead30a
Jhziel <barnedodonjaziel57@gmail.com>
2025-08-23 04:42:57 +0800

    changing query

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -22,7 +22,7 @@ const JobListings = ({ isHome = false }) => {
       }
     };
     getJobs();
-  }, []); // location.key changes every navigation
+  }, [jobs]); // location.key changes every navigation
 
   const latestJob = isHome ? jobs.slice(0, 3) : jobs;
 
